74f7abadaf07eefd23babe01a2f4b83d804dd7ed
Yulin Zhang <yuliz12@uci.edu>
2020-06-13 17:53:25 -0700

    Updates

---
+++ precache-manifest.31656569405b69144b0cdac9aeef2d82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6377542a610d5469757495539f52f2f9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "23d3809967a824baeeb0",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "32540ac14b0f9e67caa5",
+    "url": "/tic-tac-toe/static/js/2.ac17b5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.ac17b5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d3809967a824baeeb0",
+    "url": "/tic-tac-toe/static/js/main.5592b099.chunk.js"
+  },
+  {
+    "revision": "048e937512bfa9f4fb3a",
+    "url": "/tic-tac-toe/static/js/runtime-main.cb3e9845.js"
+  }
+]);
